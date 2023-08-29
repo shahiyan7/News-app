357f512798f112f2e1cdd873e9193cff5ecbce2d
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-08-29 16:02:38 +0530

    first commit

---
+++ lib/views/news_page.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:news_app/models/article_model.dart';
+
+class NewsPage extends StatefulWidget {
+  final List articles;
+  const NewsPage({
+    required this.articles,
+  });
+
+  @override
+  State<NewsPage> createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    print(widget.articles.toString());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('News Page')),
+        body: ListView.builder(
+            itemCount: widget.articles.length,
+            itemBuilder: (context, index) {
+              final article = widget.articles[index];
+              return Card(
+                child: Container(
+                  margin: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(6),
+                        child: article.urlToImage!=null?
+                        Image.network(
+                          article.urlToImage,
+                          height: 200,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                   
+                        ):CircularProgressIndicator()
+                      ),
+                     
+                      Text(
+                        article.title!=null?article.title:'No Title',
+                        style: const TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+     
+                      Text(
+                        article.description!=null?article.description:'No Description',
+                        style: const TextStyle(color: Colors.black54),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }));
+  }
+}
---
+++ lib/widgets/logos.dart
@@ -0,0 +1,14 @@
+
+
+class Logos {
+  final List<String> channelLogos = [
+    "/Users/shahiyankhan/flutterVScode/news_app/assets/images/add-icon-free-vector.jpg",
+    "/Users/shahiyankhan/flutterVScode/news_app/assets/images/360_F_179146215_NEoBhSC2oKw9hwX7K3hAkAPLZueAeGEs.jpg",
+    "/Users/shahiyankhan/flutterVScode/news_app/assets/images/bbc-news-global-logo-AAC6CDF27E-seeklogo.com.png",
+    "/Users/shahiyankhan/flutterVScode/news_app/assets/images/cnn.png",
+    "/Users/shahiyankhan/flutterVScode/news_app/assets/images/cbc.png",
+    "/Users/shahiyankhan/flutterVScode/news_app/assets/images/istockphoto-929047972-612x612.jpg",
+    "/Users/shahiyankhan/flutterVScode/news_app/assets/images/istockphoto-929047972-612x612.jpg",
+    "/Users/shahiyankhan/flutterVScode/news_app/assets/images/istockphoto-929047972-612x612.jpg",
+  ];
+}
